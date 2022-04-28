b1a098dc2452136c4589237d1d6fa5799630b927
momo698 <82819897+momo698@users.noreply.github.com>
2022-04-28 14:20:22 +0300

    Add files via upload

---
+++ Formula.java
@@ -0,0 +1,14 @@
+public class Formula {
+
+
+    public static void main(String[] args) {
+        double a= -9.81;
+        double v= 0.0;
+        double t=10.0;
+        double x = 0.0;
+        System.out.println("the object postion after" +t+ "seconds is " + 0.5*a*t*t +v*t+x +"m");
+
+    }
+
+}
+
